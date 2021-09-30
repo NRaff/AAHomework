ffbab9c8f866541aa2a101b04dc3ea6edcd6e70d
nraff <nraff510@gmail.com>
2021-09-29 22:07:02 -0700

    Rails demo project initial commit

---
+++ W5D4/demo_project/db/migrate/20210930050142_add_houses_table_to_db.rb
@@ -0,0 +1,4 @@
+class AddHousesTableToDb < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ W5D4/demo_project/db/migrate/20210930050527_add_houses_tabble_to_db_2.rb
@@ -0,0 +1,7 @@
+class AddHousesTabbleToDb2 < ActiveRecord::Migration[5.2]
+  def change
+    create_table :houses do |t|
+      t.string :address
+    end
+  end
+end

cde30ecf3ea345eb9db14116389674401a2f9af6
nraff <nraff510@gmail.com>
2021-10-04 16:25:35 -0700

    activerecord queries training

--- Studying/Rails_1/Movie-Buff-Training/movie_novice/queries.rb
+++ Studying/Rails_1/Movie-Buff-Training/movie_novice/queries.rb
@@ -23,18 +23,25 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
+  Actor.find_by(name: "Angelina Jolie")
 
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
+  Movie
+    .where("score >= ?", 9)
+    .select(:id, :title)
 
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
+  Movie
+    .where("title LIKE '%Star Wars%'")
+    .select(:id,:title,:yr)
 
 end
 
@@ -44,6 +51,11 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
+  Movie
+    .where("score < ?", 5)
+    .group(:yr)
+    .order("yr DESC")
+    .select("yr, COUNT(*) bad_movies")
 
 end
 
@@ -53,6 +65,10 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
+  Actor
+    .select(:id, :name)
+    .order("name ASC")
+    .limit(10)
 
 end
 
@@ -60,6 +76,10 @@ def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
+  Movie
+    .joins(:actors)
+    .where("movies.title = 'Pulp Fiction'")
+    .select("actors.id, actors.name")
 
 end
 
@@ -68,5 +88,10 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
+  Actor
+    .joins(:movies)
+    .where("actors.name = ?", "Uma Thurman")
+    .select("movies.id, movies.title, movies.yr")
+    .order("movies.yr ASC")
 
 end
